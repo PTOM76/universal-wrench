3c4bc4c1b9bf38796f08d0b4249012efa50c7b51
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-26 23:23:06 +0900

    add onStateReplaced

--- common/src/main/java/net/pitan76/universalwrench/block/WrenchEditTable.java
+++ common/src/main/java/net/pitan76/universalwrench/block/WrenchEditTable.java
@@ -2,6 +2,7 @@ package net.pitan76.universalwrench.block;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
@@ -9,6 +10,9 @@ import net.minecraft.util.ActionResult;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
 import net.pitan76.mcpitanlib.api.block.ExtendBlock;
 import net.pitan76.mcpitanlib.api.event.block.BlockUseEvent;
+import net.pitan76.mcpitanlib.api.event.block.ItemScattererUtil;
+import net.pitan76.mcpitanlib.api.event.block.StateReplacedEvent;
+import net.pitan76.mcpitanlib.api.util.ItemStackUtil;
 import net.pitan76.mcpitanlib.api.util.TextUtil;
 import net.pitan76.universalwrench.screen.WrenchEditTableScreenHandler;
 
@@ -36,5 +40,19 @@ public class WrenchEditTable extends ExtendBlock implements NamedScreenHandlerFa
     @Override
     public ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
         return new WrenchEditTableScreenHandler(syncId, playerInventory);
+    }
+
+    @Override
+    public void onStateReplaced(StateReplacedEvent e) {
+        if (!e.isSameState() && e.hasInventory()) {
+            Inventory inventory = (Inventory) e.getBlockEntity();
+            for (int i = 1; i < inventory.size(); i++) {
+                inventory.setStack(i, ItemStackUtil.empty());
+            }
+
+            ItemScattererUtil.spawn(e.getWorld(), e.getPos(), e.getBlockEntity());
+            e.updateComparators();
+        }
+        super.onStateReplaced(e);
     }
 }
